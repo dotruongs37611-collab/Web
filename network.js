4f2cf5b99164e411fe7979313186242b468cdb45
PFayosP <pfayosperez@gmail.com>
2025-04-14 20:43:18 +0200

    Update network.js

--- network.js
+++ network.js
@@ -300,8 +300,9 @@ document.addEventListener('DOMContentLoaded', async function () {
       if (event.key === 'Enter') {
         searchButton.click();
       }
+    });
       
-  } catch (err) {
-    console.error("Error cargando o renderizando la red:", err);
-  }
-});
+    } catch (err) {
+      console.error("Error cargando o renderizando la red:", err);
+    }
+  });
