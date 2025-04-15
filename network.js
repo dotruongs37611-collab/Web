7dbc494e841195345a656984aea50c091f767dd0
PFayosP <pfayosperez@gmail.com>
2025-04-15 20:53:05 +0200

    Update network.js

--- network.js
+++ network.js
@@ -253,7 +253,7 @@ document.addEventListener('DOMContentLoaded', async function () {
 
         const edgeFields = [
           { key: "relationship type", label: "Type of relationship" },
-          { key: "connection_level", label: "Connection_level" },
+          { key: "connection_level", label: "Type of connection" },
           { key: "correspondence", label: "Correspondence" },
           { key: "know each other since", label: "Know each other since" },
           { key: "they met", label: "They met" },
